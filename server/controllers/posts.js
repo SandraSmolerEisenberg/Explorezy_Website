f7f7d0b3fe8e3232455bf3cb8eeeceb2b12167c5
samuelg <samuelg@student.chalmers.se>
2020-09-15 10:45:26 +0200

    Add post model and posts controller, implement post Schema and post endpoints

---
+++ server/controllers/posts.js
@@ -0,0 +1,91 @@
+const { text } = require('body-parser');
+var express = require('express');
+var router = express.Router();
+
+var Post = require('../models/post');
+
+//Creates a new post object.
+router.post('/', function(req, res, next) {
+    var Post = new Post(req.body);
+    Post.save(function(err, post) {
+        if (err) { return next(err); }
+        res.status(201).json(post);
+    });
+});
+
+//Get a list of all posts with pagination.
+router.get('/', function(req, res, next) {     
+    Post.find(function(err, posts) {         
+        if (err) { return next(err); }         
+        const pageNumber = req.query.pageNumber;         
+        const limit = req.query.limit;         
+        if(pageNumber && limit){             
+            let startIndex  = (pageNumber - 1) * limit;             
+            let endIndex = pageNumber * limit;             
+            res.json(posts.slice(startIndex, endIndex));         
+        }else {             
+            res.json({'posts': posts });         
+        }     
+    }); 
+});
+
+//Get a specific post by id.
+router.get('/:id', function(req, res, next) {
+    var id = req.params.id;
+    Post.findById(id, function(err, post) {
+        if (err) { return next(err); }
+        if (post === null) {
+            return res.status(404).json({'message': 'Post not found!'});
+        }
+        res.json(post);
+    });
+});
+
+//Replaces a post by id.
+router.put('/:id', function(req, res, next) {
+    var id = req.params.id;
+    Post.findById(id, function(err, post) {
+        if (err) { return next(err); }
+        if (post === null) {
+            return res.status(404).json({'message': 'Post not found'});
+        }
+        post.title = req.body.title;
+        post.text = req.body.text;
+        post.author = req.body.author;
+        post.date = req.body.date;
+        post.save();
+        res.json(post);
+    });
+});
+
+//Updates a post by id.
+router.patch('/:id', function(req, res, next) {
+    var id = req.params.id;
+    Post.findById(id, function(err, post) {
+        if (err) { return next(err); }
+        if (post === null) {
+            return res.status(404).json({'message': 'Post not found'});
+        }
+        post.title = req.body.title || post.title;
+        post.text = req.body.text || post.text;
+        post.author = req.body.author || post.author;
+        post.date = req.body.date || post.date;
+        post.save();
+        res.json(post);
+    });
+});
+
+//Deletes a post by id.
+router.delete('/:id', function(req, res, next) {
+    var id = req.params.id;
+    Post.findOneAndDelete({_id: id}, function(err, post) {
+        if (err) { return next(err); }
+        if (post === null) {
+            return res.status(404).json({'message': 'Post not found'});
+        }
+        res.json(post);
+    });
+});
+
+
+module.exports = router;
