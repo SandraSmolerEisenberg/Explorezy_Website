fce527e6228c97f356621d90dcac468eca02f412
samuelg <samuelg@student.chalmers.se>
2020-09-25 16:26:19 +0200

    Update Home page content and add base structure for posts and places sections

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <b-jumbotron header="DIT341 Frontend" lead="Welcome to your DIT341 Frontend Vue.js App">
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
-      <p>Message from the server:<br/>
-      {{ message }}</p>
+    <b-jumbotron header="Welcome to Explorezy" lead="Explore exiting places in Gothenburg!">
     </b-jumbotron>
+      <b-row class="col-md"><b-col class="col-md-6">Left</b-col><b-col class="col-md-6">Right</b-col></b-row>
   </div>
 </template>
 
