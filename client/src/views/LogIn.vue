bfa33504a3d65babce6ddf60a0767a713739c81a
samuelg <samuelg@student.chalmers.se>
2020-09-25 17:16:09 +0200

    Remove copy, add login form

--- client/src/views/LogIn.vue
+++ client/src/views/LogIn.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <b-jumbotron header="Log In" lead="hihihoho">
+          <form>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
     </b-jumbotron>
   </div>
 </template>
