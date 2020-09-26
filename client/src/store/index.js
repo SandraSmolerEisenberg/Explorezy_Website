ffec90723a87592cb31000e8fbee6eeb2b981e29
krasen <krasenparvanov@gmail.com>
2020-09-26 14:21:20 +0200

    Add vuex store folder and index js file exprorting and registrating the account module

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import { user } from './user'
+import { account } from './account.model'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    user
+    account
   }
 })
