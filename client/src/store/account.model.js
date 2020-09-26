9f04bcfd770fe34a69142b6b885e6fd0edf59651
krasen <krasenparvanov@gmail.com>
2020-09-26 14:23:57 +0200

    Add account modul js class for vuex storage managent with coresponding actions and mutations for user login and manageing the status and user parts of the account

---
+++ client/src/store/account.model.js
@@ -0,0 +1,36 @@
+import UserService from '../services/UserService.js'
+const user = JSON.parse(localStorage.getItem('user'))
+const initialState = user
+  ? { status: { loggedIn: true }, user }
+  : { status: { loggedIn: false }, user: null }
+
+export const account = {
+  namespaced: true,
+  state: initialState,
+  actions: {
+    login({ commit }, user) {
+      return UserService.login(user).then(
+        user => {
+          commit('loginSuccess', user)
+          return Promise.resolve(user)
+        },
+        error => {
+          commit('loginFailure')
+          return Promise.reject(error)
+        }
+      )
+    }
+  },
+  mutations: {
+
+    loginSuccess(state, user) {
+      state.status.loggedIn = true
+      state.user = user
+    },
+    loginFailure(state) {
+      state.status.loggedIn = false
+      state.user = {}
+      localStorage.clear()
+    }
+  }
+}
