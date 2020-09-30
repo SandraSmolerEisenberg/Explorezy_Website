0e134191698c0b113e7ca2f387664184d46d8a94
samuelg <samuelg@student.chalmers.se>
2020-09-30 02:22:18 +0200

    Adding the places preview component

---
+++ client/src/components/places/PlacePreview.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-card>
+      <b-card-img :src=place.image class="img"></b-card-img>
+      <b-card-title>{{place.name}}</b-card-title>
+      <b-card-sub-title class="mb-2">{{place.address.road}}</b-card-sub-title>
+      <b-card-text>
+        {{place.wikipedia_extracts.text}}
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Details</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'place-preview',
+  props: ['place'],
+  methods: {
+    // Some methods
+  },
+  data() {
+    return {
+      href: 'https://commons.wikimedia.org/wiki/File:G%C3%B6teborg.jpg'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
