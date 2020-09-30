458ab58d5bebb843240e470af62fc201c45a3be5
krasen <krasenparvanov@gmail.com>
2020-09-30 09:43:48 +0200

    Update plcace service with methods to get a place by id and a method for pagination

--- client/src/services/PlacesService.js
+++ client/src/services/PlacesService.js
@@ -4,6 +4,14 @@ class PlacesService {
   async getAllPlaces() {
     return await Api.get('/places')
   }
+
+  async getPlaceByID(id) {
+    return await Api.get('/places/' + id)
+  }
+
+  async getPlacesByPage(currentPage, limit) {
+    return await Api.get('/places?page=' + currentPage + '&limit=' + limit)
+  }
 }
 
 export default new PlacesService()
