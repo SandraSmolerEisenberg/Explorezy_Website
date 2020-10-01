3aeb722ac4433dd4d2a047da62de39cf054718e2
krasen <krasenparvanov@gmail.com>
2020-10-01 10:53:44 +0200

    Implement trips page with trips compoents

--- client/src/views/Trips.vue
+++ client/src/views/Trips.vue
@@ -1,11 +1,49 @@
 <template>
+  <div>
   <b-jumbotron header="Trips" lead="Public trips!">
   </b-jumbotron>
+    <b-container>
+      <b-card-text v-show="message">{{message}}</b-card-text>
+      <b-card v-for="trip in trips" :key="trip._id">
+        <SingleTrip :trip="trip"></SingleTrip>
+      </b-card>
+    </b-container>
+
+  </div>
 </template>
 
 <script>
+import SingleTrip from '@/components/trips/SingleTrip'
+import TripService from '@/services/TripService'
 export default {
-  name: 'Trips'
+  name: 'Trips',
+  data() {
+    return {
+      trips: [],
+      message: ''
+    }
+  },
+  components: {
+    SingleTrip
+  },
+  mounted() {
+    this.getTrips()
+    this.checkTrips()
+  },
+  methods: {
+    checkTrips() {
+      if (this.trips.length <= 0) {
+        this.message = 'There are no trips, log in and add trips to the list'
+      }
+    },
+    getTrips() {
+      TripService.getAllPublicTrips().then(response => {
+        this.trips = response.data.trips
+      }).catch(() => {
+        this.message = 'Trips are not avaliable at this time'
+      })
+    }
+  }
 }
 </script>
 
