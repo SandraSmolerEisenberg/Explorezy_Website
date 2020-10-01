52e0a592dd25b98aa7c64b3f70329ac770571455
krasen <krasenparvanov@gmail.com>
2020-10-01 10:51:59 +0200

    Add axios request to backend endpoints for trips

--- client/src/services/TripService.js
+++ client/src/services/TripService.js
@@ -5,12 +5,25 @@ class TripService {
     return await Api.get('/trips')
   }
 
+  async getAllPublicTrips() {
+    return await Api.get('/trips?public=true')
+  }
+
   async getTripByID(id) {
     return await Api.get('/trips/' + id)
   }
 
-  async createTrip(trip) {
-    return await Api.post('/trips', trip)
+  async getPlaceFromTrip(tripID, placeID) {
+    return await Api.get('/trips/' + tripID + '/places/' + placeID)
+  }
+
+  async createTrip(payload) {
+    console.log(payload)
+    return await Api.post('/trips', payload)
+  }
+
+  async addPlaceToTrip(tripID, placeID) {
+    return await Api.post('/trips/' + tripID + '/places', { places: placeID })
   }
 }
 
