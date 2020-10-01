8b3864cc8aef9832bd0013e827929e6a2fb7ff67
krasen <krasenparvanov@gmail.com>
2020-10-01 14:17:20 +0200

    Add put request to update posts

--- client/src/services/PostService.js
+++ client/src/services/PostService.js
@@ -5,8 +5,16 @@ class PostService {
     return await Api.get('/posts')
   }
 
+  async getAllPostsForUser(id) {
+    return await Api.get('/posts?user=' + id)
+  }
+
   async createPost(post) {
     return await Api.post('/posts', post)
+  }
+
+  async updatePost(post) {
+    return await Api.put('/posts/' + post._id, post)
   }
 
   async deletePost(post) {
