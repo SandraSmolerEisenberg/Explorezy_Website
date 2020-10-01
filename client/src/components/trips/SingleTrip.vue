e54d863aae470b25e5b111bc0b34f3b507b1d22a
krasen <krasenparvanov@gmail.com>
2020-10-01 10:56:26 +0200

    Add place card to single trip view in order to display the places in trip

--- client/src/components/trips/SingleTrip.vue
+++ client/src/components/trips/SingleTrip.vue
@@ -5,22 +5,47 @@
       <hr/>
       <b-card-text>
         <b-card-sub-title>Places on trip</b-card-sub-title>
-        <p>//ToDo show places here</p>
+        <b-row>
+       <b-card class="card-size-my-trips" v-for="place in places" :key="place._id">
+         <b-card-header>{{place.name}}</b-card-header>
+         <b-img :src=place.image></b-img>
+         <b-card-text>{{place.wikipedia_extracts.text}}</b-card-text>
+       </b-card>
+        </b-row>
       </b-card-text>
-      <b-card-text>
       <hr/>
     </b-card>
   </div>
 </template>
 <script>
+import TripService from '@/services/TripService'
 export default {
   name: 'single-trip',
   props: ['trip'],
-  computed: {
-
+  data() {
+    return {
+      message: '',
+      places: []
+    }
+  },
+  mounted() {
+    this.getPlaces()
+  },
+  methods: {
+    getPlaces() {
+      for (let i = 0; i < this.trip.places.length; i++) {
+        TripService.getPlaceFromTrip(this.trip._id, this.trip.places[i]).then(
+          response => {
+            this.places.push(response.data.place)
+          }
+        )
+      }
+    }
   }
 }
 </script>
 <style scoped>
-
+.card-size-my-trips {
+  max-width: 30em;
+}
 </style>
