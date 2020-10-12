bc1e81d8a373b069a19620718bf0315c6204f689
SandraSmolerEisenberg <smolers@student.chalmers.se>
2020-10-12 16:21:06 +0200

    Add css file for styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,19 +21,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#navbarImg {
-  max-width: 5em;
-  height: auto;
-}
-.router-link-text{
-  color: #2c3e50 !important;
-  font-size: large;
-}
+@import "./assets/StyleSheet.css";
 </style>
